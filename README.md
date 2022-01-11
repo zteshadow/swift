3de5abf7f3a8fe85cd242c9efc606cce175e59d1
zteshadow <samuel.song.bc@gmail.com>
2022-01-11 18:55:29 +0800

    add current picker

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# swift
+# 1. RadioGroup Picker
+
+Compose a radio group picker for iOS, declared as below:
+
+```swift
+struct RadioGroupPicker<SelectionValue, Content> where SelectionValue : Hashable, Content : View
+```
+With `content: View`, not only text style but alos custom item view can be supported.
+
+## Usage:
+
+```swift
+struct RadioGroupPicker<SelectionValue, Content> where SelectionValue : Hashable, Content : View
+```
+
+## Demo
+![Tux, the Linux mascot](resource/output.gif)
--- RadioGroupPicker/RadioGroupPicker/CurrentPicker.swift
+++ RadioGroupPicker/RadioGroupPicker/CurrentPicker.swift
@@ -17,25 +17,41 @@ enum Flavor: String, CaseIterable, Identifiable {
 
 struct CurrentPicker: View {
     @State private var selectedFlavorFromMenu = Flavor.chocolate
+    @State private var selectedFlavorFromInline = Flavor.chocolate
+    @State private var selectedFlavorFromWheel = Flavor.chocolate
     @State private var selectedFlavorFromSegment = Flavor.vanilla
 
     var body: some View {
         Form {
-            Section(header: Text("Menu Style")) {
-                Picker("Flavor", selection: $selectedFlavorFromMenu) {
-                    Text("Chocolate").tag(Flavor.chocolate)
-                    Text("Vanilla").tag(Flavor.vanilla)
-                    Text("Strawberry").tag(Flavor.strawberry)
-                }.pickerStyle(.menu)
-                Text("Selected flavor: \(selectedFlavorFromMenu.rawValue)")
-            }
             Section(header: Text("Segment Style")) {
-                Picker("Flavor", selection: $selectedFlavorFromSegment) {
+                Picker("", selection: $selectedFlavorFromSegment) {
                     Text("Chocolate").tag(Flavor.chocolate)
                     Text("Vanilla").tag(Flavor.vanilla)
                     Text("Strawberry").tag(Flavor.strawberry)
                 }.pickerStyle(.segmented)
                 Text("Selected flavor: \(selectedFlavorFromSegment.rawValue)")
+            }
+            Section(header: Text("Inline Style")) {
+                Picker("", selection: $selectedFlavorFromInline) {
+                    Text("Chocolate").tag(Flavor.chocolate)
+                    Text("Vanilla").tag(Flavor.vanilla)
+                    Text("Strawberry").tag(Flavor.strawberry)
+                }.pickerStyle(.inline)
+            }
+            Section(header: Text("Wheel Style")) {
+                Picker("", selection: $selectedFlavorFromWheel) {
+                    Text("Chocolate").tag(Flavor.chocolate)
+                    Text("Vanilla").tag(Flavor.vanilla)
+                    Text("Strawberry").tag(Flavor.strawberry)
+                }.pickerStyle(.wheel).frame(maxHeight: 80)
+            }
+            Section(header: Text("Menu Style")) {
+                Picker("", selection: $selectedFlavorFromMenu) {
+                    Text("Chocolate").tag(Flavor.chocolate)
+                    Text("Vanilla").tag(Flavor.vanilla)
+                    Text("Strawberry").tag(Flavor.strawberry)
+                }.pickerStyle(.menu)
+                Text("Selected flavor: \(selectedFlavorFromMenu.rawValue)")
             }
         }
     }
