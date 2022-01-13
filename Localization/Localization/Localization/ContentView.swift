b0c53c64b2f3b8cd0352940f572b47b945253c49
Samuel Song <liasong@stubhub.com>
2022-01-13 11:15:14 +0800

    add localization project for app

---
+++ Localization/Localization/Localization/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Localization
+//
+//  Created by Liantao Song on 2022/1/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Localization/Localization/Localization/LocalizationApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalizationApp.swift
+//  Localization
+//
+//  Created by Liantao Song on 2022/1/13.
+//
+
+import SwiftUI
+
+@main
+struct LocalizationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
