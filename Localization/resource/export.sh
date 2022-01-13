36b0f9bcc3f7d92d2915317b28f42b3665dcb95d
Samuel Song <liasong@stubhub.com>
2022-01-13 15:31:58 +0800

    must use arm64

--- Localization/resource/export.sh
+++ Localization/resource/export.sh
@@ -2,16 +2,10 @@
 
 set -euo pipefail
 
-# get sdk by `xcodebuild -showsdks`
-# xcodebuild -workspace Localization.xcworkspace -scheme "Localization" -sdk iphonesimulator15.2 > /dev/null 2>&1
-# xcodebuild -workspace Localization.xcworkspace -scheme "Localization" -arch arm64
-# xcodebuild -exportLocalizations -localizationPath "export" -workspace Localization.xcworkspace -exportLanguage en
-#ARCHS=x86_64 ONLY_ACTIVE_ARCH=NO
-
 xcrun xcodebuild ARCHS=arm64 ONLY_ACTIVE_ARCH=NO \
   -workspace "Localization.xcworkspace" \
   -scheme "Localization" \
-  -configuration Debug
+  -configuration Debug > /dev/null 2>&1
 
 xcrun xcodebuild \
   -workspace "Localization.xcworkspace" \
